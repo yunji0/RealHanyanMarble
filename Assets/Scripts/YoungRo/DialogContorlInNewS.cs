0963a3559dff23b5238eb8f19789ad5d7975b9ae
0Roo <97582268+0Roo@users.noreply.github.com>
2022-02-07 14:57:22 +0900

    미니게임 기본 설정
    미니게임 기본

---
+++ Assets/Scripts/YoungRo/DialogContorlInNewS.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+
+[System.Serializable]
+public class Dialogue2
+{
+    [TextArea]
+    public string dialogue2;
+    public Sprite cg2;
+
+}
+public class DialogContorlInNewS : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _Sprite2;
+    [SerializeField] private Text _Txt2;
+    [SerializeField] private Dialogue[] dialogues2;
+    private bool isDialogue = false;
+
+    public int cnt2 = 0;
+
+    //showDialogue trigger ¸¸µé°Í
+    public void ShowDialogue()
+    {
+
+        _Sprite2.gameObject.SetActive(true);
+        _Txt2.gameObject.SetActive(true);
+        cnt2 = 0;
+        isDialogue = true;
+        NextDialogue();
+
+    }
+
+    private void NextDialogue()
+    {
+        _Txt2.text = dialogues2[cnt2].dialogue;
+        _Sprite2.sprite = dialogues2[cnt2].cg;
+        cnt2++;
+
+    }
+
+    void HideDialoge()
+    {
+        _Sprite2.gameObject.SetActive(false);
+        _Txt2.gameObject.SetActive(false);
+        cnt2 = 0;
+        isDialogue = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isDialogue)
+        {
+            if (OVRInput.GetDown(OVRInput.Button.Two) || Input.GetKeyDown(KeyCode.H))
+            {
+                if (cnt2 < dialogues2.Length)
+                { NextDialogue(); }
+                else
+                {
+                    HideDialoge();
+                    ALL.Interactflag = true;// ÈÄ¿¡ ¿ÀÇÂ ¾À
+                }
+            }
+
+        }
+
+    }
+}
+
