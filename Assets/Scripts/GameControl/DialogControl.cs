28c7ea07fa1e6ff3d1709a2279cc09bf1d84dccb
yunji0 <yunwlghks1022@gmail.com>
2022-02-10 15:58:10 +0900

    ㅓ

--- Assets/Scripts/GameControl/DialogControl.cs
+++ Assets/Scripts/GameControl/DialogControl.cs
@@ -16,6 +16,9 @@ public class DialogControl : MonoBehaviour
     [SerializeField] private SpriteRenderer _Sprite;
     [SerializeField] private Text _Txt;
     [SerializeField] private Dialogue[] dialogues;
+    [SerializeField] private GameObject YesB;
+    [SerializeField] private GameObject NoB;
+    [SerializeField] private GameObject EndB;
     private bool isDialogue = false;
 
     public int cnt = 0;
@@ -25,6 +28,7 @@ public class DialogControl : MonoBehaviour
         _Txt.gameObject.SetActive(true);
         cnt = 0;
         isDialogue = true;
+        
         NextDialogue();
     
     }
@@ -33,6 +37,17 @@ public class DialogControl : MonoBehaviour
     {
         _Txt.text = dialogues[cnt].dialogue;
         _Sprite.sprite = dialogues[cnt].cg;
+        if (cnt == 0)
+        {
+            YesB.SetActive(true);
+            NoB.SetActive(true);
+            EndB.SetActive(false);
+        }
+        else {
+            YesB.SetActive(false);
+            NoB.SetActive(false);
+            EndB.SetActive(true);
+        }
         cnt++;
     
     }
@@ -40,6 +55,7 @@ public class DialogControl : MonoBehaviour
     void HideDialoge() {
         _Sprite.gameObject.SetActive(false);
         _Txt.gameObject.SetActive(false);
+        EndB.SetActive(false);
         cnt = 0;
         isDialogue = false;
 
@@ -48,7 +64,7 @@ public class DialogControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+       /*
         if (isDialogue)
         {
             if (OVRInput.GetDown(OVRInput.Button.Two)||Input.GetKeyDown(KeyCode.H))
@@ -56,11 +72,29 @@ public class DialogControl : MonoBehaviour
                 if (cnt < dialogues.Length)
                 { NextDialogue(); }
                 else { HideDialoge(); 
+
                     ALL.Interactflag = true;// ÈÄ¿¡ ¿ÀÇÂ ¾À
+                
                 }
             }
         
-        }
+        }*/
         
+    }
+
+    public void YesButton() {
+        HideDialoge();
+
+        ALL.Interactflag = true;
+        //ÈÄ¿¡ ¿ÀÇÂ ¾À
+    }
+
+    public void NoButton() {
+        NextDialogue();
+    }
+
+    public void EndButton() {
+        HideDialoge();
+        ALL.Idleflag = true;
     }
 }
