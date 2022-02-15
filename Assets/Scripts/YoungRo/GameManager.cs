aec2e1b5ff9a1a9a327c10121bba81b70f1e353c
0Roo <97582268+0Roo@users.noreply.github.com>
2022-02-15 15:13:41 +0900

    FindScene 시작 종료 Script
    FindScene 시작 종료 Script

--- Assets/Scripts/YoungRo/GameManager.cs
+++ Assets/Scripts/YoungRo/GameManager.cs
@@ -6,16 +6,29 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    // 비비를 받을 위치
+    public GameObject GetPosition;
+    // 비비 오브젝트
+    public GameObject BiBi;
+    // 종료 cavas
+    public GameObject EndCav;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        EndCav.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        // 비비가 손에 들어왔을 때
+        if (BiBi.transform.position == GetPosition.transform.position)
+        {
+            // 충돌 감지를 위해 Rigidbody 컴포넌트 값 받아오기
+            //Rigidbody rb = BiBi.GetComponent<Rigidbody>();
+            EndCav.SetActive(true);
+        }
     }
-}
+}  
