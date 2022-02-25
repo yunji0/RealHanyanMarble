0193aab20d14a711a5e91faf9669115dd91670e1
0Roo <97582268+0Roo@users.noreply.github.com>
2022-02-26 02:26:15 +0900

    Find Scene Collider수정
    Find Scene Collider수정

---
+++ Assets/Scripts/YoungRo/AudioEffect.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioEffect : MonoBehaviour
+{
+    private AudioSource Click;
+    public AudioClip Shuck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Click = GetComponent<AudioSource>();    
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Click.clip = Shuck;
+            Click.Play();
+        }
+    }
+}
